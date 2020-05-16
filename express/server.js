77b28b7f860e1f9d34b2f4034ddfe2cc188f356b
tang wanyu <tangwanyu@tangdeiMac.local>
2020-05-17 07:44:31 +0800

    edite

--- express/server.js
+++ express/server.js
@@ -21,8 +21,8 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/api/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-app.use('/demo', (req, res) => res.sendFile(path.join(__dirname, '../demo.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
