b6d5fcd587f41bab40dbad6147aff3b0d38b14a0
tang wanyu <tangwanyu@tangdeiMac.local>
2020-05-17 05:28:03 +0800

    add demo

--- express/server.js
+++ express/server.js
@@ -11,6 +11,11 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+router.get('/demo', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js! demo</h1>');
+  res.end();
+});
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
