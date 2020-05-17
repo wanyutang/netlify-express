22c4d149a5afb7b51d9dd203d94cd4600498bc55
tang wanyu <tangwanyu@tangdeiMac.local>
2020-05-17 08:07:52 +0800

    edite

--- express/routers/auth.js
+++ express/routers/auth.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const express = require('express');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
--- express/server.js
+++ express/server.js
@@ -5,26 +5,11 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-// const routerAuth = require('./routers/auth');
-
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/demo', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js! demo</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.get('/demores', (req, res) => res.json({ user: "demo" ,pwdcode: "1qaz2wsx" }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+const routerAuth = require('./routers/auth');
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/api', router);
+app.use('/.netlify/functions/server', routerAuth);  // path must route to lambda
+app.use('/api', routerAuth);
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
