59d57cc4778193c5f8ba57fa360e95934b1ad218
tang wanyu <tangwanyu@tangdeiMac.local>
2020-05-17 08:09:41 +0800

    edite

--- express/server.js
+++ express/server.js
@@ -9,6 +9,7 @@ const routerAuth = require('./routers/auth');
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', routerAuth);  // path must route to lambda
+app.use('/.netlify/auth', routerAuth); 
 app.use('/api', routerAuth);
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
